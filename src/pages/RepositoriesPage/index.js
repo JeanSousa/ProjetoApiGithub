0780305e57bb41c86c3a0e09ad6ec1335bf5fc09
JeanSousa <jeanjr.silvasousa@gmail.com>
2021-11-15 19:12:02 -0300

    finish filters

--- src/pages/RepositoriesPage/index.js
+++ src/pages/RepositoriesPage/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Profile from './Profile';
 import Filter from './Filter';
@@ -8,6 +8,9 @@ import { Container, Sidebar, Main } from './styles';
 import { getLangsFrom } from '../../services/api';
 
 const RepositoriesPage = () => {
+
+  const [currentLanguage, setCurrentLanguage] = useState()
+
   const user = {
     login: "JeanSousa",
     name: "Jean Sousa",
@@ -21,42 +24,49 @@ const RepositoriesPage = () => {
 
   const repositories = [
     {
+      id: '1',
       name: "API_Rest_Laravel",
       description: 'Descricao',
       html_url: "https://github.com/JeanSousa/API_Rest_Laravel",
       language: "PHP"
     },
     {
+      id: '2',
       name: "CarCrmReact",
       description: 'Descricao',
       html_url: "https://github.com/JeanSousa/CarCrmReact",
       language: "Javascript"
     },
     {
+      id: '3',
       name: "fisioSystem",
       description: 'Descricao',
       html_url: "https://github.com/JeanSousa/fisioSystem",
       language: "PHP"
     },
     {
+      id: '4',
       name: "Todo Ruby",
       description: 'Descricao',
       html_url: "https://github.com/JeanSousa/fisioSystem",
       language: "Ruby"
     },
     {
+      id: '5',
       name: "Schedule",
       description: 'Descricao',
       html_url: "https://github.com/JeanSousa/fisioSystem",
       language: "Java"
     },
     {
+      id: '6',
       name: "Todo",
       description: 'Descricao',
       html_url: "https://github.com/JeanSousa/fisioSystem",
       language: "Typescript"
     },
     {
+      id: '7',
       name: "Todo",
       description: 'Descricao',
       html_url: "https://github.com/JeanSousa/fisioSystem",
@@ -66,15 +76,25 @@ const RepositoriesPage = () => {
 
   const languages = getLangsFrom(repositories);
 
+  const onFilterClick = (language) => {
+    setCurrentLanguage(language);
+  }
+
 
   return (
     <Container>
       <Sidebar>
         <Profile user={user} />
-        <Filter languages={languages} />
+        <Filter languages={languages}
+        currentLanguage={currentLanguage}
+        onClick={onFilterClick}
+        />
       </Sidebar>
       <Main>
-        <Repositories />
+        <Repositories
+        repositories={repositories}
+        currentLanguage={currentLanguage}
+        />
       </Main>
     </Container>
   )
